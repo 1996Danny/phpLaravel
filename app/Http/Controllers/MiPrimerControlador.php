9c9acd74cf557830df041d4cd0abdbd8f579f72d
Daniel Frías <friasdaniel996@gmail.com>
2025-06-06 14:33:54 -0300

    rutas, conexion con bd

--- app/Http/Controllers/MiPrimerControlador.php
+++ app/Http/Controllers/MiPrimerControlador.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class MiPrimerControlador extends Controller
 {
-    //
+    public function raiz()
+    {
+        return "Hola mundo desde el controlador";
+    }
+    public function saludo()
+        {
+            return "Hola mundo";
+        }
+    public function misNoticias ()
+        {
+            return "Noticias de la semana";
+        }
+    public function miHistoria ()
+    {
+        return "Historia de la empresa";
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MiPrimerControlador;
 
-Route::get('/', function () {
-    return 'Hola mundo';
-});
+// Route::get('/', function () {
+//     return 'Hola mundo';
+// });
 
-Route::get('/saludo', function (){
-    return 'Buenos días';
-});
+// Route::get('/saludo', function (){
+//     return 'Buenos días';
+// });
+
+Route::get('/', [MiPrimerControlador::class, 'raiz']);
+Route::get('/saludo', [MiPrimerControlador::class, 'saludo']);
+Route::get('/misNoticias', [MiPrimerControlador::class, 'misNoticias']);
+Route::get('/miHistoria', [MiPrimerControlador::class, 'miHistoria']);
